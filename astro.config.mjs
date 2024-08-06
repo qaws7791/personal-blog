18019611ba79ddec3c94d1903a25dad276c9e9d3
qaws7791 <qaws7791@gmail.com>
2024-08-06 23:06:58 +0900

    Update astro.config.mjs with remark-mermaid and remark-github-callouts plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,17 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
 
+import remarkMermaid from "remark-mermaidjs";
+import { remarkGithubCallouts } from "./src/plugins/remark-github-callouts";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-micro.vercel.app",
-  integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
     shikiConfig: {
       theme: "css-variables",
     },
+    remarkPlugins: [remarkGithubCallouts, remarkMermaid],
   },
+  integrations: [tailwind(), sitemap(), mdx(), pagefind()],
 });
