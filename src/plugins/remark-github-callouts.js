656651b302b0ba87888475b183a84c3477e4fd5d
qaws7791 <qaws7791@gmail.com>
2024-08-06 23:06:16 +0900

    Add GithubCallout component and plugin

---
+++ src/plugins/remark-github-callouts.js
@@ -0,0 +1,29 @@
+import { visit } from "unist-util-visit";
+
+export function remarkGithubCallouts() {
+  return (tree) => {
+    visit(tree, "blockquote", (node, index, parent) => {
+      console.log("node", node);
+      const [firstChild] = node.children;
+      if (firstChild && firstChild.type === "paragraph") {
+        const [firstInline] = firstChild.children;
+        if (firstInline && firstInline.type === "text") {
+          const match = firstInline.value.match(
+            /^\[!(NOTE|TIP|IMPORTANT|WARNING|CAUTION)\]/i,
+          );
+          if (match) {
+            const type = match[1].toLowerCase();
+            node.type = "html";
+            node.value = `<callout type=${type}>
+            ${firstInline.value.slice(match[0].length)}\n${node.children
+              .slice(1)
+              .map((child) => toMarkdown(child))
+              .join("\n")}
+            </callout>`;
+            return [visit.SKIP, index + 1];
+          }
+        }
+      }
+    });
+  };
+}
