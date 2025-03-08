978c2505d409703c52f5f5edbe91eb9d5b343533
qaws7791 <qaws7791@gmail.com>
2025-03-08 23:31:25 +0900

    QUOTES 상수에 여러 인용 추가

--- src/consts.ts
+++ src/consts.ts
@@ -29,4 +29,20 @@ export const SOCIALS: Socials = [
   },
 ];
 
-export const QUOTES = ["Everything is amazing and nobody is happy - Louis C K"];
+export const QUOTES = [
+  "Everything is amazing and nobody is happy - Louis C K",
+  "Program testing can be used to show the presence of bugs, but never to show their absence! - Edsger W. Dijkstra",
+  "KISS - Keep it simple, stupid!",
+  "DRY - Don't repeat yourself - Andy Hunt and Dave Thomas",
+  "YAAGNI - You aren't gonna need it - Ron Jeffries",
+  "Premature optimization is the root of all evil - Donald Knuth",
+  "The only way to go fast is to go well - Robert C. Martin",
+  "Programs must be written for people to read, and only incidentally for machines to execute. - Harold Abelson",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand. - Martin Fowler",
+  "The best programs are written so that computing machines can perform them quickly and so that human beings can understand them clearly - Donald Knuth",
+  "Make it work, Make it right, Make it fast - Kent Beck",
+  "A language that doesn't affect the way you think about programming is not worth knowing - Alan J. Perlis",
+  "The most important property of a program is whether it accomplishes the intention of its user. - C.A.R. Hoare",
+  "Programmers are not to be measured by their ingenuity and their logic but by the completeness of their case analysis. - Alan J. Perlis",
+  "The most dangerous phrase in the language is 'we've always done it this way.' - Grace Hopper",
+];
