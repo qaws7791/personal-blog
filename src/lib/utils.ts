142f7d179982fa1cc9ed3a3facbf197039f69551
qaws7791 <qaws7791@gmail.com>
2025-04-03 22:56:30 +0900

    읽기 시간 메시지 형식 수정

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -16,5 +16,5 @@ export function readingTime(html: string) {
   const textOnly = html.replace(/<[^>]+>/g, "");
   const wordCount = textOnly.split(/\s+/).length;
   const readingTimeMinutes = (wordCount / 200 + 1).toFixed();
-  return `${readingTimeMinutes}분이면 읽을 수 있어요.`;
+  return `${readingTimeMinutes}분 소요`;
 }
